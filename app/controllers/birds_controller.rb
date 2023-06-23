7cb7d44a93beea54192bf33b20c675b51a2869d3
malcomowilla <owillamalcom@gmail.com>
2023-06-23 14:48:51 +0300

    solved

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,16 @@
+class BirdsController < ApplicationController
+
+def index
+    birds = Bird.all
+    render json: birds, except: [:created_at, :updated_at]
+end
+def show
+bird = Bird.find_by(id: params[:id])
+if bird
+    render json: bird
+else
+    render json: {error: "Bird not found"}, status: :not_found 
+end
+end
+
+end
